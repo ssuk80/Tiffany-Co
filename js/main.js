a4a3bca5c7307fdcd0c5dd4caf3d25f1be2f6b18
ssuk80 <xkftjs@gmail.com>
2023-04-23 17:46:11 +0900

    비주얼

--- js/main.js
+++ js/main.js
@@ -47,3 +47,26 @@ btnClose.addEventListener('click', function () {
 
   mobileNavEl.classList.remove('active');
 });
+
+
+
+// 프로모션 수평 슬라이드 기능
+new Swiper ('.visual .swiper', {
+  direction:'horizontal',//수평 슬라이드(기본값)
+  loop: true,//반복 재생 여부
+  autoplay:{
+    delay : 5000 //5초마다 슬라이드 바뀜(기본값:3000)
+  }, //자동재생여부
+  slidesPerView: 3,//한번에 보여줄 슬라이드 개수(기본값:1)
+  centeredSlides: true,//1번 슬라이스가 가운데 보이기
+
+  pagination: {
+    el:'.visual .swiper-pagination' , //페이지 번호 요소 선택자
+    clickable: true //사용자 페이지 번호 요소 제어 가능 여부
+  }, //페이지 번호 사용
+  navigation: { //슬라이드 이전/다음 버튼
+    nextEl:'.visual .swiper-button-next',
+    prevEl:'.visual .swiper-button-prev',
+  },
+
+});
